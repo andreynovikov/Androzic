6804f81caa0447cf2628a69c7de40e639378fab9
Andrey Novikov <novikov@gmail.com>
2012-01-22 17:14:07 +0400

    Added JavaDoc

--- src/com/androzic/util/TDateTime.java
+++ src/com/androzic/util/TDateTime.java
@@ -6,22 +6,21 @@ import java.util.TimeZone;
 
 /**
  * Converts time in different formats to Delphi (Pascal) <code>TDateTime</code> and vice versa.
- * Take in mind that <code>TDateTime</code> is internaly represented as <code>double</code>.
+ * Take in mind that <code>TDateTime</code> is internally represented as <code>double</code>:
  * 
+ * <p>The integral part of a TDateTime value is the number of days that have passed since
+ * 12/30/1899. The fractional part of a TDateTime value is the time of day.
+ * Following are some examples of TDateTime values and their corresponding dates and times:
+ * <p>0 - 12/30/1899 0:00<br/>
+ * 2.75 - 1/1/1900 18:00<br/>
+ * -1.25 - 12/29/1899 6:00<br/>
+ * 35065 - 1/1/1996 0:00
+ *
  * @author Andrey Novikov
  */
 public class TDateTime
 {
 	/*
-	 * Delphi stores date and time values in the TDateTime type. The integral
-	 * part of a TDateTime value is the number of days that have passed since
-	 * 12/30/1899. The fractional part of a TDateTime value is the time of day.
-	 * Following are some examples of TDateTime values and their corresponding
-	 * dates and times:
-	 * 0 - 12/30/1899 0:00
-	 * 2.75 - 1/1/1900 18:00
-	 * -1.25 - 12/29/1899 6:00
-	 * 35065 - 1/1/1996 0:00
 	 */
 
 	/**
@@ -42,21 +41,21 @@ public class TDateTime
 	}
 
 	/**
-	 * Converts conventional Unix milliseconds to TDateTime format
+	 * Converts {@link java.util.Date Date} to TDateTime format
 	 * 
-	 * @param time milliseconds since January 1, 1970
+	 * @param date time in Date format
 	 * @return time in TDateTime format
 	 */
 	public static double toDateTime(final Date date)
 	{
 		Calendar zero = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 		//12/30/1899 0:00
-		zero.set(1899, 12, 30, 0, 0, 0);
+		zero.set(1899, Calendar.DECEMBER, 30, 0, 0, 0);
+		zero.clear(Calendar.MILLISECOND);
 		Calendar from = Calendar.getInstance();
 		from.setTime(date);
 
-        // 2678400946L - http://stackoverflow.com/questions/8911190
-        return (from.getTimeInMillis() - zero.getTimeInMillis() + 2678400946L) / 86400000.0;
+        return (from.getTimeInMillis() - zero.getTimeInMillis()) / 86400000.0;
 	}
 
 	/**
@@ -71,10 +70,10 @@ public class TDateTime
 	}
 
 	/**
-	 * Converts TDateTime time to Date object
+	 * Converts TDateTime time to {@link java.util.Date Date}
 	 * 
 	 * @param time in TDateTime format
-	 * @return java.util.Date
+	 * @return {@link java.util.Date Date}
 	 */
 	public static Date dateFromDateTime(double time)
 	{
