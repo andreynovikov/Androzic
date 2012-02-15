50c340f930fc39bd0104475856e2614bc16875d9
Andrey Novikov <novikov@gmail.com>
2012-02-16 00:28:20 +0400

    Fixed commit error

--- src/com/androzic/location/LocationService.java
+++ src/com/androzic/location/LocationService.java
@@ -490,12 +490,6 @@ public class LocationService extends Service implements LocationListener, NmeaLi
 		 * lastKnownLocation.setLongitude(29.451150);
 		 */
 
-		 lastKnownLocation.setSpeed(20); lastKnownLocation.setBearing(340);
-		 lastKnownLocation.setAltitude(169);
-		 lastKnownLocation.setLatitude(55.852527);
-		 lastKnownLocation.setLongitude(29.451150);
-		 
-
 		if (useCompass && lastKnownLocation.getSpeed() == 0)
 		{
 			location.setBearing(azimuth);
