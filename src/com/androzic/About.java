5b9f8835c74db88777886430c0a7266d3942c0cd
Andrey Novikov <novikov@gmail.com>
2014-10-15 13:07:38 +0400

    Easter egg

--- src/com/androzic/About.java
+++ src/com/androzic/About.java
@@ -24,7 +24,9 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.Html;
@@ -34,6 +36,7 @@ import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class About extends DialogFragment
@@ -100,7 +103,7 @@ public class About extends DialogFragment
 		super.onDestroyView();
 	}
 
-	private void updateAboutInfo(View view)
+	private void updateAboutInfo(final View view)
 	{
 		// version
 		String versionName = null;
@@ -128,14 +131,14 @@ public class About extends DialogFragment
 		links.append("\">");
 		links.append(getString(R.string.faq));
 		links.append("</a><br /><a href=\"");
-		links.append(getString(R.string.featureuri));	
+		links.append(getString(R.string.featureuri));
 		links.append("\">");
 		links.append(getString(R.string.feedback));
 		links.append("</a>");
 		final TextView homelinks = (TextView) view.findViewById(R.id.homelinks);
 		homelinks.setText(Html.fromHtml(links.toString()));
 		homelinks.setMovementMethod(LinkMovementMethod.getInstance());
-		
+
 		// community links
 		StringBuilder communities = new StringBuilder();
 		communities.append("<a href=\"");
@@ -154,7 +157,7 @@ public class About extends DialogFragment
 		final TextView communitylinks = (TextView) view.findViewById(R.id.communitylinks);
 		communitylinks.setText(Html.fromHtml(communities.toString()));
 		communitylinks.setMovementMethod(LinkMovementMethod.getInstance());
-		
+
 		// donations
 		StringBuilder donations = new StringBuilder();
 		donations.append("<a href=\"");
@@ -166,11 +169,11 @@ public class About extends DialogFragment
 		donations.append("\">");
 		donations.append(getString(R.string.donate_paypal));
 		donations.append("</a>");
-		
+
 		final TextView donationlinks = (TextView) view.findViewById(R.id.donationlinks);
 		donationlinks.setText(Html.fromHtml(donations.toString()));
 		donationlinks.setMovementMethod(LinkMovementMethod.getInstance());
-		
+
 		Androzic application = Androzic.getApplication();
 		if (application.isPaid)
 		{
@@ -197,10 +200,49 @@ public class About extends DialogFragment
 			credits.append(merits[i]);
 			credits.append("</b> &mdash; ");
 			credits.append(names[i]);
-			credits.append("<br />");			
+			credits.append("<br />");
 		}
-		
+
 		final TextView creditlist = (TextView) view.findViewById(R.id.credits);
 		creditlist.setText(Html.fromHtml(credits.toString()));
+
+		// dedication
+		final TextView dedicated = (TextView) view.findViewById(R.id.dedicated);
+		dedicated.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v)
+			{
+				dedicated.setVisibility(View.GONE);
+				View photo = view.findViewById(R.id.photo);
+				photo.setVisibility(View.VISIBLE);
+				photo.setOnClickListener(redirect);
+			}
+		});
 	}
+
+	private int clicks = 1;
+
+	private View.OnClickListener redirect = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v)
+		{
+			clicks++;
+			if (clicks > 3)
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW,  Uri.parse("http://andreynovikov.info/galleries/slideshow.xhtml?-filt.starred=1;-filt.labels=7;theme=classic;any=1")));
+				clicks = -1;
+			}
+			else if (clicks == 0)
+			{
+				v.setVisibility(View.GONE);
+				clicks++;
+			}
+			else
+			{
+				((ImageView) v).setImageResource(getResources().getIdentifier("olya" + clicks, "drawable" , getActivity().getPackageName()));
+			}
+		}};
+
 }
