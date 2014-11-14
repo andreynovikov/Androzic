ce316fc114d558a18a7751081ebdfa9a6bdf1c31
Andrey Novikov <novikov@gmail.com>
2014-11-15 01:05:51 +0300

    Pretty suitable maps list

--- src/com/androzic/SuitableMapsList.java
+++ src/com/androzic/SuitableMapsList.java
@@ -1,12 +1,12 @@
 package com.androzic;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -15,17 +15,21 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.androzic.map.Map;
 
 public class SuitableMapsList extends DialogFragment implements OnItemClickListener
 {
-	private final static String KEY_NAME = "NAME";
-	private final static String KEY_DESC = "DESC";
-
 	private ListView listView;
-	private List<com.androzic.map.Map> maps;
-	private OnMapActionListener mapActionsCallback;
+	private OnMapActionListener mapActionsCallback;	
+	private SuitableMapListAdapter adapter;
+
+	private List<Map> maps;
+	private String mapsPath;
+	private Map currentMap;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -45,8 +49,14 @@ public class SuitableMapsList extends DialogFragment implements OnItemClickListe
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		View view = inflater.inflate(R.layout.list_with_empty_view, container);
+		View view = inflater.inflate(R.layout.dlg_suitable_maps_list, container);
 		listView = (ListView) view.findViewById(android.R.id.list);
+		View infoButton = view.findViewById(R.id.information_button);
+		infoButton.setOnClickListener(onMapInformation);
+		View editButton = view.findViewById(R.id.edit_button);
+		editButton.setOnClickListener(onMapEdit);
+		View openButton = view.findViewById(R.id.open_button);
+		openButton.setOnClickListener(onMapOpen);
 		return view;
 	}
 
@@ -75,26 +85,11 @@ public class SuitableMapsList extends DialogFragment implements OnItemClickListe
 		Androzic application = Androzic.getApplication();
 		double[] loc = application.getMapCenter();
 		maps = application.getMaps(loc);
-		List<Map<String, String>> mapData = new ArrayList<Map<String, String>>();
 
-		Map<String, String> group;
+		currentMap = application.getCurrentMap();
+		mapsPath = application.getMapPath();
 
-		String mappath = application.getMapPath();
-
-		for (com.androzic.map.Map map : maps)
-		{
-			String fn = new String(map.mappath);
-			if (fn.startsWith(mappath))
-			{
-				fn = fn.substring(mappath.length() + 1);
-			}
-			group = new HashMap<String, String>();
-			group.put(KEY_NAME, map.title);
-			group.put(KEY_DESC, String.format("MPP: %.2f - %s", map.mpp, fn));
-			mapData.add(group);
-		}
-
-		SimpleAdapter adapter = new SimpleAdapter(getActivity(), mapData, android.R.layout.simple_list_item_2, new String[] { KEY_NAME, KEY_DESC }, new int[]{ android.R.id.text1, android.R.id.text2 } );
+		adapter = new SuitableMapListAdapter(getActivity());
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(this);
 	}
@@ -114,4 +109,112 @@ public class SuitableMapsList extends DialogFragment implements OnItemClickListe
 		dismiss();
 	}
 
+	public class SuitableMapListAdapter extends BaseAdapter
+	{
+		private Context mContext;
+		private LayoutInflater mInflater;
+		private int mItemLayout;
+
+		public SuitableMapListAdapter(Context context)
+		{
+			mContext = context;
+			mItemLayout = R.layout.list_item_suitable_map;
+			mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public Map getItem(int position)
+		{
+			return maps.get(position);
+		}
+
+		@Override
+		public long getItemId(int position)
+		{
+			return maps.get(position).id;
+		}
+
+		@Override
+		public int getCount()
+		{
+			return maps.size();
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			View v;
+			if (convertView == null)
+			{
+				v = mInflater.inflate(mItemLayout, parent, false);
+			}
+			else
+			{
+				v = convertView;
+			}
+			Map map = getItem(position);
+			TextView title = (TextView) v.findViewById(R.id.name);
+			title.setText(map.title);
+			TextView scale = (TextView) v.findViewById(R.id.scale);
+			scale.setText(String.format("%.0f%% (%.0f)", 100 * currentMap.mpp / map.mpp, map.mpp));
+			TextView path = (TextView) v.findViewById(R.id.filename);
+			if (map.mappath != null)
+			{
+				String filepath = map.mappath.startsWith(mapsPath) ? map.mappath.substring(mapsPath.length() + 1, map.mappath.length()) : map.mappath;
+				path.setText(filepath);
+			}
+			else
+			{
+				path.setText("");
+			}
+
+			Resources resources = mContext.getResources();
+			int color;
+			if (map.id == currentMap.id)
+			{
+				color = resources.getColor(R.color.drawer_selected_text);
+				title.setTypeface(Typeface.DEFAULT_BOLD);
+			}
+			else
+			{
+				color = resources.getColor(android.R.color.primary_text_dark);
+				title.setTypeface(Typeface.DEFAULT);
+			}
+			title.setTextColor(color);
+			scale.setTextColor(color);
+			path.setTextColor(color);
+
+			return v;
+		}
+
+		@Override
+		public boolean hasStableIds()
+		{
+			return true;
+		}
+	}
+
+	private View.OnClickListener onMapInformation = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v)
+		{
+		}
+	};
+
+	private View.OnClickListener onMapEdit = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v)
+		{
+		}
+	};
+
+	private View.OnClickListener onMapOpen = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v)
+		{
+		}
+	};
 }
---
+++ src/com/androzic/overlay/HillShadeOverlay.java
@@ -0,0 +1,120 @@
+package com.androzic.overlay;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.androzic.Androzic;
+import com.androzic.MapView;
+import com.androzic.map.Map;
+
+public class HillShadeOverlay extends MapOverlay
+{
+	private Paint paint;
+	private RandomAccessFile demReader;
+
+	public HillShadeOverlay()
+	{
+		super();
+		paint = new Paint();
+		paint.setAntiAlias(false);
+		paint.setStrokeWidth(1);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setColor(Color.BLACK);
+		try
+		{
+	    	Androzic application = Androzic.getApplication();
+			File demFile = new File(application.dataPath, "N44E034.hgt");
+			demReader = new RandomAccessFile(demFile, "r");
+			enabled = true;
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public synchronized void onMapChanged()
+	{
+	}
+
+	@Override
+	public void onPreferencesChanged(SharedPreferences settings)
+	{
+	}
+
+	@Override
+	public void onBeforeDestroy()
+	{
+		super.onBeforeDestroy();
+		try
+		{
+			demReader.close();
+			demReader = null;
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	//TODO This should go into Application
+	private int getHeight(double latitude, double longitude)
+	{
+		int lat = (int) Math.floor(latitude);
+		int lon = (int) Math.floor(longitude);
+		int x3 = (int) ((latitude - lat) * 1200);
+		int y3 = (int) ((longitude - lon) * 1200);
+		if (demReader != null)
+		{
+			try
+			{
+				demReader.seek(x3 * 1201 * 2 + y3 * 2);
+				int height = demReader.readShort();
+				return height;
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Integer.MIN_VALUE;
+	}
+
+	@Override
+	public void onPrepareBuffer(final MapView.Viewport viewport, final Canvas c)
+	{
+		Androzic application = Androzic.getApplication();
+		final int[] cxy = viewport.mapCenterXY;
+		cxy[0] -= viewport.width / 2;
+		cxy[1] -= viewport.height / 2;
+    	Map map = application.getCurrentMap();
+		int w = viewport.width;
+		int h = viewport.height;
+    	double[] cll = new double[2];
+		for (int x = 0; x < w; x++)
+			for (int y = 0; y < h; y++)
+			{
+		    	map.getLatLonByXY(cxy[0] + x, cxy[1] + y, cll);
+		    	int height = getHeight(cll[0], cll[1]);
+				paint.setAlpha(height);
+		    	c.drawPoint(x - viewport.width / 2, y - viewport.height / 2, paint);
+			}
+	}
+
+	@Override
+	public void onPrepareBufferEx(final MapView.Viewport viewport, final Canvas c)
+	{
+	}
+
+}
